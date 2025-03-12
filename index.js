f3aae3a8945a301504f717d9bab57de72b3e9c42
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-12 22:37:42 +0600

    mongodb setup & dotenv setup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +11,42 @@ app.use(express.json());
 app.get('/', (req, res) =>{
     res.send('Coffee Store is Running......')
 })
+
+// const uri = "mongodb+srv://CoffeeManager:0yrpwuexC5Uzsgf4@cluster0.hvhc0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hvhc0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(uri)
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, ()=>{
     console.log(`Server is Running : ${port}`)
